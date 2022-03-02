bb525c52b303a45d8fc683637f13f79c9a258347
Goryachenok <goryachev.na72@gmail.com>
2022-03-03 01:50:57 +0500

    adaptive_main

--- js/main.js
+++ js/main.js
@@ -1,7 +1,29 @@
 
-    var swiper = new Swiper(".mySwiper", {
-    navigation: {
-    nextEl: ".slider_navigation_next",
-    prevEl: ".slider_navigation_prev",
+    var swiper = new Swiper(".mySwiper_first", {
+        slidesPerView: 1,
+        spaceBetween: 30,
+        loop: true,
+        navigation: {
+        nextEl: ".slider_navigation_next",
+        prevEl: ".slider_navigation_prev",
+        },
+        breakpoints:{
+        500:
+            {
+                slidesPerView: 2,
+                spaceBetween: 30,
+                loop: true,
+                navigation: {
+                    nextEl: ".slider_navigation_next",
+                    prevEl: ".slider_navigation_prev",
+            }
+        },
 },
 });
+
+//PHONE MASK
+    jQuery(function($){
+        $("#phone_mask_second").mask("+7 (999) 999-99-99");
+        $("#phone_mask").mask("+7 (999) 999-99-99");
+        //$("#phone-mask-two").mask("+7 (999) 999-99-99");
+    });

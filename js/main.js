01f31f9a6d3bca66a6d82280ca44ca5705a3b75f
Goryachenok <goryachev.na72@gmail.com>
2022-03-03 16:03:38 +0500

    third_block_tw_pr

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-
-    var swiper = new Swiper(".mySwiper_first", {
+//SLIDERS
+    var swiper1 = new Swiper(".mySwiper_first", {
         slidesPerView: 1,
         spaceBetween: 30,
         loop: true,
@@ -21,9 +21,72 @@
 },
 });
 
+var swiper2 = new Swiper(".projects_slider_main", {
+    spaceBetween: 30,
+    loop: true,
+    navigation: {
+        nextEl: ".slider_project_navigation_next",
+        prevEl: ".slider_project_navigation_prev",
+    },
+});
+
+
+    var swiper3 = new Swiper(".mySwiper", {
+    loop: true,
+    spaceBetween: 10,
+    slidesPerView: 3,
+    freeMode: true,
+    watchSlidesProgress: true,
+        scrollbar: {
+            el: ".swiper-scrollbar",
+        },
+});
+    var swiper4 = new Swiper(".mySwiper2", {
+    loop: true,
+    spaceBetween: 10,
+    navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+},
+    thumbs: {
+    swiper: swiper3,
+},
+});
+
+var swiper5 = new Swiper(".projects_slider_four", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    loop: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+    },
+});
+
+
+
+
+
+
+
+
+
 //PHONE MASK
     jQuery(function($){
         $("#phone_mask_second").mask("+7 (999) 999-99-99");
         $("#phone_mask").mask("+7 (999) 999-99-99");
         //$("#phone-mask-two").mask("+7 (999) 999-99-99");
     });
+
+
+$(".inpit_email").on("keypress", function(e) {
+
+    var char = /["a-zA-Z]/;
+    var val = String.fromCharCode(e.which);
+    var test = char.test(val);
+
+    if(!test) return false
+})
